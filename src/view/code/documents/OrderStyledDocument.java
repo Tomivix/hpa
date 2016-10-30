efee8baf3a922c23f1082fa673ac5d3020a8b80a
Andrzej <andrzej4172.lama@gmail.com>
2016-10-30 12:38:35 +0100

    TEMPORARY link between GUI and parsing method

--- src/view/code/documents/OrderStyledDocument.java
+++ src/view/code/documents/OrderStyledDocument.java
@@ -7,19 +7,21 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
-import placeholder.Placeholder;
+import pseudo.Engine;
 import view.code.NumHeader;
 
 public class OrderStyledDocument extends ColorStyledDocument {
 	private static final long serialVersionUID = 1L;
 	
-	private AttributeSet defStyle, labelStyle, dirStyle;
+	private AttributeSet defStyle, labelStyle, orderStyle, paramStyle, invStyle;
 	public OrderStyledDocument(NumHeader numHeader) {
 		super(numHeader);
 		StyleContext cont = StyleContext.getDefaultStyleContext();
 		defStyle = cont.addAttribute(cont.addAttribute(cont.getEmptySet(), StyleConstants.FontFamily, "Monospaced"), StyleConstants.FontSize, 18);
 		labelStyle = cont.addAttribute(cont.addAttribute(defStyle, StyleConstants.Italic, true), StyleConstants.Foreground, Color.ORANGE);
-		dirStyle = cont.addAttribute(cont.addAttribute(defStyle, StyleConstants.Bold, true), StyleConstants.Foreground, Color.CYAN);
+		orderStyle = cont.addAttribute(cont.addAttribute(defStyle, StyleConstants.Bold, true), StyleConstants.Foreground, Color.CYAN);
+		paramStyle = cont.addAttribute(defStyle, StyleConstants.Bold, true);
+		invStyle = cont.addAttribute(defStyle, StyleConstants.Foreground, Color.red);
 		dFont = cont.getFont(defStyle);
 		numHeader.updateHeader(dFont);
 	}
@@ -27,12 +29,18 @@ public class OrderStyledDocument extends ColorStyledDocument {
 	public void recalculateStyles(){
 		try{
 			super.setCharacterAttributes(0, super.getLength(), defStyle, true);
-			int[][] indexes = Placeholder.getIndexes2(super.getText(0, super.getLength()));
+			int[][] indexes = new Engine().split(super.getText(0, super.getLength()));
 			for(int i = 0; i < indexes[0].length; i++){
 				super.setCharacterAttributes(indexes[0][i], indexes[0][++i], labelStyle, true);
 			}
 			for(int i = 0; i < indexes[1].length; i++){
-				super.setCharacterAttributes(indexes[1][i], indexes[1][++i], dirStyle, true);
+				super.setCharacterAttributes(indexes[1][i], indexes[1][++i], orderStyle, true);
+			}
+			for(int i = 0; i < indexes[2].length; i++){
+				super.setCharacterAttributes(indexes[2][i], indexes[2][++i], paramStyle, true);
+			}
+			for(int i = 0; i < indexes[3].length; i++){
+				super.setCharacterAttributes(indexes[3][i], indexes[3][++i], invStyle, true);
 			}
 		}catch (BadLocationException e) {
 			e.printStackTrace();
