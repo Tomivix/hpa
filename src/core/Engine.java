97b2c0382b33245a79e2e6e3391fe6c3cf336399
Tomivix <tomivix@wp.pl>
2016-11-01 23:20:55 +0100

    Cleaning the code and removing its obsolete parts

--- src/core/Engine.java
+++ src/core/Engine.java
@@ -3,10 +3,8 @@ package core;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
 
 public class Engine {
@@ -16,6 +14,8 @@ public class Engine {
 	public static final byte POSITIVE = 1;
 	public static final byte NEGATIVE = 2;
 	public static final byte ERROR = 3;
+	
+	// HEAD //
 	
 	private final String[] cmdRR = {
 			"AR", "SR", "MR", "DR", "CR", "LR"
@@ -33,9 +33,11 @@ public class Engine {
 				"DC", "DS"
 	};
 		
-	private final String[][] commands = {  // <-- what is that array supposed to do?
+	private final String[][] commands = {
 				cmdRR, cmdRM, cmdJ, cmdM
 	};
+	
+	// TAIL // <-- what are all those arrays for?
 	
 	//all the rest
 	
@@ -62,7 +64,7 @@ public class Engine {
 		mathOps = new String[]{"A","S","M","D","C"};
 
 		regs = new int[16];
-		// for(int reg : regs) reg = 0;  <-- registers should start with random value
+		for(int reg : regs) reg = (new Random()).nextInt();
 		regs[14] = lastVar = 1024;
 		regs[15] = lastOrder = currentOrder = 2048;
 
@@ -78,8 +80,7 @@ public class Engine {
 		addOrder("Siea", 2);
 		addOrder("YOYO", 4);
 		return orders.toString() + lastOrder;
-	}
-	
+	}	
 	
 	public int getReg(int id){
 		return regs[id];
@@ -105,8 +106,7 @@ public class Engine {
 	public void addOrder(String order, int size){
 		orders.put(lastOrder, order);
 		lastOrder += size;
-	}
-	
+	}	
 	
 	public void setCurrentOrder(int id){
 		this.currentOrder = id;
@@ -121,84 +121,66 @@ public class Engine {
 		else if(value < 0) flag = NEGATIVE;
 		else flag = ZERO;
 	}
-	
-	
+		
 	private abstract class Function {
 		public abstract void execute(int arg1, int arg2);
-	}
-	
+	}	
 	
 	private void createFunctions(){
 		
 		// register - register functions -------------------------------------------//		
 		
-		// bulk function for adding AR,SR,MR,DR,CR arithmetic operations
-		// takes less space at the cost of unnoticeably longer compiling time
-		
 		for(final String op : mathOps) {
 			functions.put(op.concat("R"), new Function() {
 				public void execute(int arg1, int arg2) {
-					int reg = 0;
-					switch(op) {
+					int reg = 0; switch(op) {
 						case "A": reg = getReg(arg1) + getReg(arg2); break;
 						case "S": reg = getReg(arg1) - getReg(arg2); break;
 						case "M": reg = getReg(arg1) * getReg(arg2); break;
 						case "D": reg = getReg(arg1) / getReg(arg2); break;
 						case "C": reg = getReg(arg1) - getReg(arg2); break;
-					}
-					if(!op.equals("C")) setReg(arg1, reg); setFlag(reg);
+					} if(!op.equals("C")) setReg(arg1, reg); setFlag(reg);
 				}
 			});
 		}
 		
 		functions.put("LR", new Function(){
 			public void execute(int arg1, int arg2){
-				int reg = getReg(arg2);
-				setReg(arg1, reg);
+				setReg(arg1, getReg(arg2));
 			}
-		});
+		});		
 		
-		
-		
-		// register - memory functions -------------------------------------------//
-		
-		// bulk function for adding A,S,M,D,C arithmetic operations
-		// takes less space at the cost of unnoticeably longer compiling time 
+		// register - memory functions -------------------------------------------// 
 		
 		for(final String op : mathOps) {
 			functions.put(op, new Function() {
 				public void execute(int arg1, int arg2) {
-					int reg = 0;
-					switch(op) {
+					int reg = 0; switch(op) {
 						case "A": reg = getReg(arg1) + getVar(arg2); break;
 						case "S": reg = getReg(arg1) - getVar(arg2); break;
 						case "M": reg = getReg(arg1) * getVar(arg2); break;
 						case "D": reg = getReg(arg1) / getVar(arg2); break;
 						case "C": reg = getReg(arg1) - getVar(arg2); break;
-					}
-					if(!op.equals("C")) setReg(arg1, reg); setFlag(reg);
+					} if(!op.equals("C")) setReg(arg1, reg); setFlag(reg);
 				}
 			});
 		}
 		
 		functions.put("L", new Function(){
 			public void execute(int arg1, int arg2){
-				int reg = getVar(arg2);
-				setReg(arg1, reg);
+				setReg(arg1, getVar(arg2));
 			}
 		});
 		
 		functions.put("LA", new Function(){
 			public void execute(int arg1, int arg2){
-				int reg = arg2;
-				setReg(arg1, reg);
+				setReg(arg1, arg2);
 			}
 		});
 		
 		functions.put("ST", new Function(){
 			public void execute(int arg1, int arg2){
-				int reg = getReg(arg1);
-				setVar(arg2, reg);
+				setVar(arg2, getReg(arg1));
 			}
 		});
 		
@@ -228,135 +210,32 @@ public class Engine {
 			public void execute(int arg1, int arg2){
 				if(getFlag() == ZERO) setCurrentOrder(arg1);
 			}
-		});
-		
+		});		
 		
 		// memory functions -------------------------------------------//
 		
 		functions.put("DC", new Function(){
 			public void execute(int arg1, int arg2){
-				for(int i=0; i<arg1; i++)
-					addVar(arg2);
+				for(int i=0; i<arg1; i++) addVar(arg2);
 			}
 		});
 		
 		functions.put("DS", new Function(){
 			public void execute(int arg1, int arg2){
-				for(int i=0; i<arg1; i++){
-					int value = (new Random()).nextInt();
-					addVar(value);
-				}
+				for(int i=0; i<arg1; i++)
+					addVar((new Random()).nextInt());
 			}
-		});
-		
+		});		
 	}
-	/*
-	// public boolean parse(String line) returns boolean
-	// checks if the raw command provided by user has correct HPA-compliant syntax
-	// returns true only if the command has valid syntax (can possibly be executed)
-	// this method (so far) doesn't check if specified memory cells were declared!
-	// previous label checking regex ([A-Z0-9]+ : |^)
-	public int parse(String line, boolean mode) {
-		int type = 0;
-		if(!mode) {
-			type = (line.matches("([A-Z][A-Z0-9]* : |^)(AR|SR|MR|DR|CR|LR) [0-9]+,[0-9]+")) ? 1 : type;
-			type = (line.matches("([A-Z][A-Z0-9]* : |^)(A|S|M|D|C|L|LA|ST) [0-9]+,([A-Z][A-Z0-9]*|[0-9]+\\([0-9]+\\))")) ? 2 : type;
-			type = (line.matches("([A-Z][A-Z0-9]* : |^)(J|JN|JP|JZ) ([A-Z][A-Z0-9]*|[0-9]+\\([0-9]+\\))")) ? 3 : type;
-		} else {
-			type = (line.matches("[A-Z][A-Z0-9]* : DC ([0-9]+\\*|)(INTEGER|INT)\\(-?[0-9]+\\)")) ? 4 : type;
-			type = (line.matches("[A-Z][A-Z0-9]* : DS ([0-9]+\\*|)(INTEGER|INT)")) ? 5 : type;
-		}
-		return type;
-	}
-	
-	// takes string line and type of command from parse() (subject to change)
-	// returns array res containing start/end index of each part depending on type
-	// (0 - possible label) 1 - command name 2 - first parameter (3 - second parameter)
-	// -1 means no match was found (part with [-1,-1] return doesn't exist in given string)
-	public int[][] getPos(String line, boolean mode) {		
-		int temp; int res[][] = new int[4][2];		
-		for(int[] sub : res) Arrays.fill(sub, -1);
-		int type; if((type = this.parse(line, mode)) == 0) {
-			this.flag = ERROR; return res;
-		} 
-		if((temp = line.indexOf(" : ") + 1) != 0) {
-			res[0][1] = temp - 2; res[0][0] = 0;
-			temp += 2; line = line.substring(temp);
-		}
-		res[1][1] = line.indexOf(" ") + temp - 1;
-		res[1][0] = temp; res[2][0] = res[1][1] + 2;
-		if(type < 3) {
-			res[2][1] = line.indexOf(",") + temp - 1;
-			res[3][1] = line.length() + temp - 1;
-			res[3][0] = res[2][1] + 2;
-		} else if(type > 3) { int temp2; 
-			if((temp2 = line.indexOf("*") - 1) != -2)
-			res[2][1] = temp + temp2; else res[2][0] = -1;
-			if(type == 4) {				
-				res[3][1] = line.indexOf(")") + temp - 1;
-				res[3][0] = line.indexOf("(") + temp + 1;
-			}
-		} else res[2][1] = line.length() + temp - 1;
-		return res;
-	}
-	
-	// takes all the lines of the code and returns int[][] containing indexes
-	// return syntax (different from sugested by Aicedosh to match above functions)
-	// int[0] = {Label1Index, Label1Length, Label2Index, Label2Length, ...}
-	// int[1] = {Order1Index, Order1Length, Order2Index, Order2Length, ...}
-	// int[2] = {Parameter1Index, Parameter1Length, Parameter2Index, Parameter2Length, ...}
-	// int[3] = {InvalidLine1Index, InvalidLine1Length, InvalidLine2Index, InvalidLine2Length, ...}
-	public int[][] split(String raw, boolean mode) {
-		if(raw.length() == 0) return new int[][]{new int[]{},new int[]{},new int[]{},new int[]{}};
-        List<List<Integer>> temp = new ArrayList<List<Integer>>();
-        for(int i = 0; i < 4; i++) temp.add(new ArrayList<Integer>());
-		String[] lines = raw.split("\n"); int last = 0;
-		for(String line : lines) {
-			if(line.length() == 0) {last++; continue;} 
-			if(this.parse(line, mode) == 0) {
-				temp.get(3).add(last); temp.get(3).add(line.length());
-			} else {
-				int[][] res = this.getPos(line, mode);
-				for(int i = 0; i < 4; i++) {
-					if(!Arrays.equals(res[i], new int[]{-1,-1})) {
-						int j = (i == 3) ? 2 : i;
-						temp.get(j).add(res[i][0] + last);
-						temp.get(j).add(res[i][1] - res[i][0] + 1);
-					}
-				}
-			} last += line.length() + 1;
-		} int[][] res = new int[4][];
-		for(int i = 0; i < 4; i++) {
-			res[i] = new int[temp.get(i).size()]; int j = 0;
-			for(Integer pos : temp.get(i)) res[i][j++] = pos;				
-		} return res;
-	}*/
-	
-	/* not working for now	
-	public boolean split(String line) {
-		String[] res = new String[4];
-		int temp;
-		if((temp = line.indexOf(" : ")) != -1) {
-			res[0] = line.substring(0,--temp) + "";
-		}
-		return true;
-	}*/
 	
 	// dummy main method, remove if no longer needed
 	public static void main(String[] args) throws IOException {
-		Engine Engine = new Engine();
-		Engine.setReg(1, 123);
-		Engine.setReg(2, 17);
-		Engine.functions.get("AR").execute(1, 2);
-        
-		 // code below doesn't work anymore - offset by 1
+		
+	// all the code below doesn't work anymore - offset by 1
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line; String raw = "";
 
-        while ((line = in.readLine()) != null && !line.equals("END")) {
-        	raw += line + "\n";
-        }
-        int[][] res = Parser.split(raw, false);
-        System.out.println(Arrays.deepToString(res));
+        while ((line = in.readLine()) != null && !line.equals("END")) raw += line + "\n";
+        int[][] res = Parser.split(raw, false); System.out.println(Arrays.deepToString(res));
 	}
 }
